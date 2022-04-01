db13eb4066fc21d9b3f6f45baa29e3108817ce6d
lautarodapin-magoya <lautaro@magoya.com>
2022-03-31 22:20:09 -0300

    Cors

--- main.go
+++ main.go
@@ -12,11 +12,28 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CorsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	godotenv.Load()
 
 	models.ConnectDatabase()
 	r := gin.Default()
+	// r.Use(CorsMiddleware())
 	docs.SwaggerInfo.BasePath = "/"
 
 	kanbans := r.Group("/kanbans")
