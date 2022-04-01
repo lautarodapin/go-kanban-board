c6c3fb7636281471276ac4d5e48e1c69746b430c
lautarodapin-magoya <lautaro@magoya.com>
2022-04-01 18:40:31 -0300

    Not null fields

--- models/models.go
+++ models/models.go
@@ -4,32 +4,32 @@ import "time"
 
 type Kanban struct {
 	BaseModel
-	Name      string    `json:"name" gorm:"type:varchar(255)"`
+	Name      string    `json:"name" gorm:"type:varchar(255);not null"`
 	StartDate time.Time `json:"start_date" gorm:"not null"`
 	EndDate   time.Time `json:"end_date" gorm:"not null"`
 }
 
 type Column struct {
 	BaseModel
-	Name      string     `json:"name" gorm:"type:varchar(255)"`
+	Name      string     `json:"name" gorm:"type:varchar(255);not null"`
 	Order     uint       `json:"order" gorm:"type:integer;not null;default:0;unique;"`
 	Dropzones []Dropzone `json:"dropzones" gorm:"foreignkey:ColumnID"`
 }
 
 type Dropzone struct {
 	BaseModel
-	Name     string `json:"name" gorm:"type:varchar(255)"`
-	ColumnID uint   `json:"column_id" gorm:"type:integer"`
+	Name     string `json:"name" gorm:"type:varchar(255);not null"`
+	ColumnID uint   `json:"column_id" gorm:"type:integer;not null"`
 	Order    uint   `json:"order" gorm:"type:integer;not null;default:0;"`
 	Column   Column `json:"column" gorm:"foreignkey:ColumnID"`
 }
 
 type Ticket struct {
 	BaseModel
-	Title       string   `json:"title" gorm:"type:varchar(255)"`
+	Title       string   `json:"title" gorm:"type:varchar(255);not null"`
 	Description string   `json:"description" gorm:"type:varchar(1025)"`
 	KanbanID    uint     `json:"kanban_id" gorm:"type:integer"`
-	DropzoneID  uint     `json:"dropzone_id" gorm:"type:integer"`
+	DropzoneID  uint     `json:"dropzone_id" gorm:"type:integer; not null"`
 	Kanban      Kanban   `json:"kanban" gorm:"foreignkey:KanbanID"`
 	Dropzone    Dropzone `json:"dropzone" gorm:"foreignkey:DropzoneID"`
 }
