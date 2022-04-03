2261b87904612e339bd82f0216f56c1ca450814f
lautarodapin-magoya <lautaro@magoya.com>
2022-04-02 22:48:17 -0300

    Migrate to async

--- boards/router.py
+++ boards/router.py
@@ -10,14 +10,15 @@ api = APIRouter(prefix='/boards')
 
 
 @api.get('/', response_model=list[Board])
-async def get_boards():
-    async with AsyncSession(async_engine) as session:
-        results = (await session.exec(select(Board))).fetchall()
-    return results
+async def get_boards(session: AsyncSession = Depends(get_async_session)):
+    return (await session.exec(select(Board))).fetchall()
 
 
 @api.get('/{board_id}', response_model=Board)
-async def get_board(board_id: int, session: AsyncSession = Depends(get_async_session)):
+async def get_board(
+    board_id: int,
+    session: AsyncSession = Depends(get_async_session),
+):
     result = (await session.exec(select(Board).where(
         Board.id == board_id))).first()
     if result:
@@ -26,33 +27,40 @@ async def get_board(board_id: int, session: AsyncSession = Depends(get_async_ses
 
 
 @api.post('/', response_model=Board)
-async def create_board(board: CreateBoard):
-    with Session(engine) as session:
-        board_db = Board.from_orm(board)
-        session.add(board_db)
-        session.commit()
-        session.refresh(board_db)
+async def create_board(
+    board: CreateBoard,
+    session: AsyncSession = Depends(get_async_session),
+):
+    board_db = Board.from_orm(board)
+    session.add(board_db)
+    await session.commit()
+    await session.refresh(board_db)
     return board_db
 
 
 @api.put('/{board_id}', response_model=Board)
-async def update_board(board_id: int, board: Board):
-    with Session(engine) as session:
-        board_db = session.get(Board, board_id)
-        if board_db is None:
-            raise HTTPException(404, 'Board not found')
+async def update_board(
+    board_id: int,
+    board: Board,
+    session: AsyncSession = Depends(get_async_session),
+):
+    board_db = await session.get(Board, board_id)
+    if board_db is None:
+        raise HTTPException(404, 'Board not found')
 
-        for k, v in board.dict(exclude_unset=True).items():
-            setattr(board_db, k, v)
-        session.add(board_db)
-        session.commit()
-        session.refresh(board_db)
+    for k, v in board.dict(exclude_unset=True).items():
+        setattr(board_db, k, v)
+    session.add(board_db)
+    await session.commit()
+    await session.refresh(board_db)
     return board_db
 
 
 @api.delete('/{board_id}')
-async def delete_board(board_id: int):
-    with Session(engine) as session:
-        board = session.exec(select(Board).where(Board.id == board_id)).one()
-        session.delete(board)
-        session.commit()
+async def delete_board(
+    board_id: int,
+    session: AsyncSession = Depends(get_async_session),
+):
+    board = (await session.exec(select(Board).where(Board.id == board_id))).one()
+    await session.delete(board)
+    await session.commit()
