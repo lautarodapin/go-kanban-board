4fcc91e78af0e308cda6e7e76f07652bfa2655b4
lautarodapin-magoya <lautaro@magoya.com>
2022-04-03 20:23:52 -0300

    Add required board foreign key

--- tickets/models.py
+++ tickets/models.py
@@ -18,14 +18,14 @@ class BaseTicket(SQLModel):
     title: str
     description: Optional[str]
     dropzone_id: Optional[int] = Field(None, foreign_key='dropzone.id')
+    board_id: int = Field(foreign_key='board.id')
     type: TicketType = Field(sa_column=Column(Enum(TicketType)))
 
 
 class Ticket(BaseTicket, table=True):
     id: Optional[int] = Field(None, primary_key=True)
-
     dropzone: Optional["Dropzone"] = Relationship(back_populates="tickets")
-    board: Optional["Board"] = Relationship(back_populates="tickets")
+    board: "Board" = Relationship(back_populates="tickets")
 
 
 class CreateTicket(BaseTicket):
